6f62db541b215010d54ecf0c6d922ee021bd97d1
brightk1977 <brightk1977@student.faytechcc.edu>
2025-06-16 14:23:51 +0000

    initial commit

---
+++ P1LAB1_BrightKristopher.py
@@ -0,0 +1,9 @@
+# Kristopher Bright	
+# 6/16/2025
+# P1LAB1
+# This project takes user input and replies with a personalized message based on input
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
+
