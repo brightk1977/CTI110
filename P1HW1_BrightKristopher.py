03f54770754a3c33d1fa6418bed47558d3f26857
brightk1977 <brightk1977@student.faytechcc.edu>
2025-06-16 16:17:51 +0000

    first commit

---
+++ P1HW1_BrightKristopher.py
@@ -0,0 +1,24 @@
+# Kristopher Bright
+# 6/16/2025
+# P1HW1
+# A simple math calculator
+
+print("-----Calculating Exponenets----""\n")
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print()
+result = base ** exponent
+
+print(base, "raised to the power of", exponent, "is", result, "\n")
+
+print("-----Addition and Subtraction----""\n")
+
+first = int(input("Enter a starting integer: "))
+second = int(input("Enter an integer to add: "))
+third = int(input("Enter an integer to subtract: "))
+print()
+finalResult = first + second - third
+
+print(first, "+", second, "-", third, "is equal to", finalResult)
+
